0ef6ddb1272b60d1abd6c57c4bc00b919597ab8d
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-08-06 23:26:13 -0300

    primeira letra do texto em maiuscula

--- dinamica.js
+++ dinamica.js
@@ -1,4 +1,6 @@
-let comunidade="idev"
-console.log(comunidade)
-let texto = "Comunidade - Dinâmica em grupo"
-console.log(texto)
+let comunidade="idev";
+console.log(comunidade);
+let texto = "comunidade - Dinâmica em grupo";
+console.log(texto);
+texto = texto[0].toUpperCase() + texto.slice(1, texto.length);
+console.log(texto);
