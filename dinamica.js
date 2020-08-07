60b4c3c2e2291fadc7c8c9f135f9c851e075e823
Vinicius Emidio <vinicius.s.emidio@gmail.com>
2020-08-06 23:36:52 -0300

    texteCase, data

--- dinamica.js
+++ dinamica.js
@@ -1,6 +1,13 @@
-let comunidade="idev";
+let comunidade = "idev";
 console.log(comunidade);
 let texto = "comunidade - Dinâmica em grupo";
 console.log(texto);
 texto = texto[0].toUpperCase() + texto.slice(1, texto.length);
 console.log(texto);
+let texto1 = texto.toLowerCase()
+console.log(texto1)
+let data = new Date()
+console.log(data)
+let dia = data.getDay()
+let mes = data.getMonth()
+console.log(`${dia} / ${mes}`)
